13e88438f2c8c5d2a54844d810a2309c6ad67bbf
skimanikings <kimanisimon856@gmail.com>
2019-12-02 16:35:46 +0300

    add a timeAgo pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { QuoteFormComponent } from './quote-form/quote-form.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 import { DateCountPipe } from './date-count.pipe';
 import { HighlightDirective } from './highlight.directive';
+import {TimeAgoPipe} from 'time-ago-pipe'
 
 
 
@@ -20,6 +21,7 @@ import { HighlightDirective } from './highlight.directive';
     QuoteDetailComponent,
     DateCountPipe,
     HighlightDirective,
+    TimeAgoPipe,
     
   
   ],
