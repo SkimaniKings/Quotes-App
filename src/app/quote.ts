93f99e99d72e9f405b3b20f150415cd5ff23ad01
skimanikings <kimanisimon856@gmail.com>
2019-12-02 04:12:35 +0300

    create a new quote-detail component

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,19 @@
 export class Quote {
+    showDescription: boolean;
+  //numberOfDislikes: any;
+  // numberOfDislikes: number;
+constructor(
+public id: number,
+public name: string,
+public description: string,
+//public theQuote: string,
+public author: string,
+public postDate:Date,
+public numberOfLikes:number, 
+public numberOfDislikes:number
+)
+{
+  this.showDescription=false;
 }
+}
+
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,69 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  public quotes: Quote[] = [
+    new Quote(1, "Kogei", "As a young boy, I was taught in high school that hacking was cool.", "Kevin mitnick", new Date(2019, 10, 27), 0, 0),
+    new Quote(2, "Kogei", "Technology is a word that describes something that doesn’t work yet.", "Douglas Adams", new Date(2019, 10, 27), 0, 0),
+    new Quote(3, "Kogei", "The hacking trend has definitely turned criminal because of e-commerce.", "Elbert Hubbard", new Date(2019, 10, 27), 0, 0),
+    new Quote(4, "Kogei", " “It's not a faith in technology. It's faith in people.”.", "John Lasseter", new Date(2019, 10, 27), 0, 0),
+  ]
+
+
+
+
+
+
+
+  
+  addNewQuote(quote) {
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength + 1;
+    this.quotes.push(quote)
+  }
+
+  toggleDetails(index) {
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
+
+  quoteDelete(isComplete, index) {
+    if (isComplete) {
+      let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}?`)
+
+      if (toDelete) {
+        this.quotes.splice(index, 1)
+      }
+    }
+  }
+
+  numberOfLikes : number=0;
+  numberOfDislikes =0;
+
+  likeButtonClick(i) {
+    this.quotes[i].numberOfLikes++;
+  }
+
+  
+
+  dislikeButtonClick(i) {
+    this.quotes[i].numberOfDislikes++;
+  }
+
+  preNum:number
+  lastNum:number
+  counter:number
+
+  highestUpvote(){
+    this.preNum = 0
+    this.lastNum = 0
+
+    for(this.counter=0 ; this.counter < this.quotes.length; this.counter++) {
+      this.lastNum = this.quotes[this.counter].numberOfLikes;
+      if(this.lastNum > this.preNum){this.preNum = this.lastNum}
+    }
+    return  this.preNum
+  }
+
 
   constructor() { }
 
