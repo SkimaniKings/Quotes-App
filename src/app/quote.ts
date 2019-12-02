3b40bb9efd5d7f0e610bf17fb47ba8a6037909d3
skimanikings <kimanisimon856@gmail.com>
2019-12-02 08:45:02 +0300

    change color of the quotes

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,12 +1,12 @@
 export class Quote {
     showDescription: boolean;
-  //numberOfDislikes: any;
-  // numberOfDislikes: number;
+ 
 constructor(
 public id: number,
 public name: string,
 public description: string,
-//public theQuote: string,
+
+
 public author: string,
 public postDate:Date,
 public numberOfLikes:number, 
