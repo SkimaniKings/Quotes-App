548792d801225a7be39f10af7506b3ab49f4c7df
skimanikings <kimanisimon856@gmail.com>
2019-12-03 08:05:25 +0300

    remove time ago pipe

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,10 +9,10 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   public quotes: Quote[] = [
-    new Quote(1, "Skimani", "Don't Wait for opportunity. Create it.", "Skimanikings", new Date(2019, 11, 1), 0, 0),
-    new Quote(2, "Skimani", "If you want to see the rainbow you have to put up with the rain", "Peserverance", new Date(2019, 11, 1), 0, 0),
-    new Quote(3, "Skimani", "Technology is a useful servant but a dangerous master.", "Technology", new Date(2019, 11, 1), 0, 0),
-    new Quote(4, "Skimani", " “The grass is always greener when you water it”.", "Peserverance", new Date(2019, 11, 1), 0, 0),
+    new Quote(1, "Opportunity", "Don't Wait for opportunity. Create it.", "Mburia", new Date(2019, 11, 1), 0, 0),
+    new Quote(2, "Perseverance", "If you want to see the rainbow you have to put up with the rain", "Skimankings", new Date(2019, 11, 1), 0, 0),
+    new Quote(3, "Technology", "Technology is a useful servant but a dangerous master.", "Hammerton", new Date(2019, 11, 1), 0, 0),
+    new Quote(4, "Determination", " “The grass is always greener when you water it”.", "Angelo", new Date(2019, 11, 1), 0, 0),
   ]
   
   addNewQuote(quote) {
